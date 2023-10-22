171bda95617fa77073994cf284a87919a754cd7d
emsi06001 <emsi06001@E-PW0234N3>
2023-10-22 12:34:38 +0200

    Add project files.

---
+++ inluppg4.12/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace inluppg4_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bestäm kvadraternas sidlängd");
+            int sideWidth = int. Parse(Console.ReadLine());
+            Console.WriteLine("Bestäm rutäntets bredd");
+            int gridWidth = int.Parse(Console.ReadLine());
+            Console.WriteLine("Bestäm rutnätets höjd");
+            int gridHeight = int.Parse(Console.ReadLine());
+
+            for (int  i = 0; i < gridHeight; i++)
+            {
+                for (int va = 0; va < sideWidth; va++)
+                {
+                    for (int j = 0; j < gridWidth; j++)
+                    {
+                        for (int k = 0; k < sideWidth; k++)
+                        {
+                            Console.Write('*');
+                        }
+                        Console.Write(' ');
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
